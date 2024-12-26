c8ef0758baff0b9b073448493597b115e469821f
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-12-26 13:58:13 +0100

    add user route to index

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
 
+const { userRouter } = require('./routes/userRoutes')
+
 const mongoose = require("mongoose")
 const mongoDB = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_SERVER + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
 async function main() {
@@ -15,9 +17,11 @@ main().catch(err => console.log(err));
 app.use(cors())
 app.use(express.json())
 
+app.use('/user', userRouter)
+
 const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
-    console.log("El usuario de la base de datos es: ", process.env.DB_USER)
+    console.log("The database user is: ", process.env.DB_USER)
 });
 
 module.exports = { app, server };
