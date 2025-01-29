a3284fba97b91efbe0e609ccd7985cb218d33a38
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-01-29 19:48:44 +0100

    creo gettrainingid

--- routes/trainingRoutes.js
+++ routes/trainingRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getTraining, updateTraining, addTraining, deleteTraining } = require("../controllers/trainingController");
+const { getTraining, updateTraining, addTraining, deleteTraining, getTrainingId } = require("../controllers/trainingController");
 
 const trainingRouter = express.Router();
 
-trainingRouter.get('/:id?', getTraining)
-trainingRouter.put('/:id?', updateTraining)
+trainingRouter.get('/', getTraining)
+trainingRouter.get('/:id?', getTrainingId)
 trainingRouter.post('/addtraining', addTraining)
+trainingRouter.put('/:id?', updateTraining)
 trainingRouter.delete('/:id?', deleteTraining)
 
 module.exports = { trainingRouter }
